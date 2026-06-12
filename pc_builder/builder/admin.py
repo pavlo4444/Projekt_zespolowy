4ffd394799193d1f0927c7a887ec3b664df2b784
dmytro165 <dh16469@nauka.panschelm.edu.pl>
2026-06-12 14:53:49 +0200

    admin fixes

--- pc_builder/builder/admin.py
+++ pc_builder/builder/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Component, SavedBuild
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("slug", "name_pl", "sort_order")
+
+
+@admin.register(Component)
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price_pln", "power_watts")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+
+
+@admin.register(SavedBuild)
+class SavedBuildAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "total_price_pln", "updated_at")
+    search_fields = ("title", "user__username")
