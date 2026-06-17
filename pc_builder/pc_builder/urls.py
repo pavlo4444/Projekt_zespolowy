0681bf157184148d42a7833e8133e8b88f3410e1
dmytro165 <dh16469@nauka.panschelm.edu.pl>
2026-06-17 21:30:45 +0200

    urls fix

--- pc_builder/pc_builder/urls.py
+++ pc_builder/pc_builder/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
-from . import views
-
-app_name = "builder"
 urlpatterns = [
-     path("", """views.index""", name="index"),
-     path("rejestracja/", """views.register_view""", name="register"),
+    path("admin/", admin.site.urls),
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", include("builder.urls")),
 ]
